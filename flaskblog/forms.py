59aa883fd3537ab46a14c8800d46fbdb7fb495f1
msjopyho <117312312+msjopyho@users.noreply.github.com>
2023-03-25 22:33:19 -0500

    allow pagination and a user specific post page

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 import email_validator
 from flaskblog.models import User
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -54,3 +55,9 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
